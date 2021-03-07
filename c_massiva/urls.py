6d7a06776008e1553458f166a24ab74424114799
Matheus Dantas <mind@icomp.ufam.edu.br>
2021-03-07 19:11:07 -0300

    up

--- c_massiva/urls.py
+++ c_massiva/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('cmassiva/dashboard', cmassiva_index , name='cmassiva_dashboard'),
-    path('logout', logoutUser, name='logout')
+    path('logout', logoutUser, name='logout'),
+    path('cmassiva/geradorcarga', cmassiva_gerarcarga, name='cmassiva_gerarcarga')
 ]
